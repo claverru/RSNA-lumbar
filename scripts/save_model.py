126f8ae295d61d31496a50f79a025f933d020683
Claudio <claverru@gmail.com>
2024-06-19 14:42:05 +0000

    feat: train and save study ensemble

--- scripts/save_model.py
+++ scripts/save_model.py
@@ -5,9 +5,10 @@ from typing import Dict, List, Tuple
 import torch
 import tyro
 import lightning as L
+import numpy as np
 import yaml
 
-from src.study import model, constants as study_constants
+from src.study import model, constants as study_constants, data_loading
 
 
 class Ensemble(L.LightningModule):
@@ -24,25 +25,51 @@ class Ensemble(L.LightningModule):
         return result_dict
 
 
+def get_loss(checkpoint_path: Path) -> float:
+    return float(checkpoint_path.stem.split("=")[-1])
+
 def load_from_checkpoints_dir(checkpoints_dir: Path) -> Tuple[List[model.LightningModule], int]:
     modules = []
+    losses = []
     for checkpoint_path in checkpoints_dir.rglob("*.ckpt"):
         print("<----", checkpoint_path)
-        hparams_path = checkpoint_path.parent.parent / "config.yaml"
-        hparams = yaml.load(open(hparams_path), Loader=yaml.FullLoader)
-        model_kwargs = hparams["model"]["init_args"]
-        img_size = hparams["data"]["init_args"]["img_size"]
+        losses.append(get_loss(checkpoint_path))
+        config_path = checkpoint_path.parent.parent / "config.yaml"
+        config = yaml.load(open(config_path), Loader=yaml.FullLoader)
+        model_kwargs = config["model"]["init_args"]
         module = model.LightningModule.load_from_checkpoint(checkpoint_path, **model_kwargs).cpu()
         modules.append(module)
-    return modules, img_size
+    return modules, config, losses
 
 
-def save(checkpoints_dir: Path, out_path: Path):
-    modules, img_size = load_from_checkpoints_dir(checkpoints_dir)
-    print("---->", out_path)
+def save(checkpoints_dir: Path, out_dir: Path):
+    modules, config, losses = load_from_checkpoints_dir(checkpoints_dir)
     ens = Ensemble(modules)
-    x = torch.randn(1, 3, study_constants.IMGS_PER_DESC, img_size, img_size)
-    ens.to_torchscript(out_path, "trace", x, strict=False)
+
+    model_path = out_dir / "model.pt"
+    config_path = out_dir / "config.yaml"
+    metrics_path = out_dir / "metrics.yaml"
+    transforms_path = out_dir / "transforms.yaml"
+
+    img_size = config["data"]["init_args"]["img_size"]
+
+
+    out_dir.mkdir(parents=True, exist_ok=True)
+    try:
+        ens.to_torchscript(model_path)
+    except:
+        print("fallback to trace method")
+        x = torch.randn(1, 3, study_constants.IMGS_PER_DESC, img_size, img_size)
+        ens.to_torchscript(model_path, "trace", x, strict=False)
+    yaml.dump(config, open(config_path, "w"))
+    metrics = {
+        "losses": losses,
+        "mean": float(np.mean(losses)),
+        "std": float(np.std(losses))
+    }
+    yaml.dump(metrics, open(metrics_path, "w"))
+    transforms = data_loading.get_transforms(img_size)
+    yaml.dump(transforms.to_dict(), open(transforms_path, "w"))
 
 
 if __name__ == "__main__":
