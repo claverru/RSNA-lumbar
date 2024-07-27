9dd5944ae8a67229e51dc33d93c352f8c8de920c
Claudio <claverru@gmail.com>
2024-07-27 09:23:42 +0000

    feat: warmup scheduler in sequence

--- src/model.py
+++ src/model.py
@@ -1,6 +1,8 @@
+from typing import Union
 import torch
 import lightning as L
 from lightning.pytorch.cli import OptimizerCallable, LRSchedulerCallable
+from torch.optim.lr_scheduler import _LRScheduler
 
 
 class LightningModule(L.LightningModule):
@@ -38,3 +40,26 @@ def get_transformer(emb_dim, n_heads, n_layers, dropout):
     )
     encoder = torch.nn.TransformerEncoder(layer, n_layers, norm=torch.nn.LayerNorm(emb_dim), enable_nested_tensor=False)
     return encoder
+
+
+class WarmupLR(_LRScheduler):
+    def __init__(
+        self,
+        optimizer: torch.optim.Optimizer,
+        warmup_steps: Union[int, float] = 25000,
+        last_epoch: int = -1,
+    ):
+        self.warmup_steps = warmup_steps
+        super().__init__(optimizer, last_epoch)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(warmup_steps={self.warmup_steps})"
+
+    def get_lr(self):
+        step_num = self.last_epoch + 1
+        return [
+            lr
+            * self.warmup_steps**0.5
+            * min(step_num**-0.5, step_num * self.warmup_steps**-1.5)
+            for lr in self.base_lrs
+        ]
