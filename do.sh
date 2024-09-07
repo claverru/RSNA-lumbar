a3f662a567f6205e7eefe3adae8543017826f6bd
Claudio <claverru@gmail.com>
2024-09-07 09:59:14 +0000

    fix: typo

--- do.sh
+++ do.sh
@@ -1,6 +1,6 @@
 # data
 python scripts/parse_images.py --in-dir data/train_images --out-dir data/train_images_clip
-python scripts/create_metadata.py --out-path data/metadata.csv --no-normalize --no-fillna
+python scripts/create_metadata_df.py --out-path data/metadata.csv --no-normalize --no-fillna
 python scripts/create_spinal_new_coordnates.py
 
 
